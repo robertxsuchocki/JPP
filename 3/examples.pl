2bc191f1d4071d924535cf4e9a640f65e52c5a2a
Robert Suchocki <rs361086@students.mimuw.edu.pl>
2018-06-02 17:06:54 +0200

    Working full version with answer multiplication largely reduced, style changes and comments left to do

--- 3/examples.pl
+++ 3/examples.pl
@@ -1,13 +1,18 @@
-jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B).
-jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], [[v0, a, v1, v3],[v1, e, v2],[v2, a],[v3, a]]).
-jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], [[v1, e, v2],[v0, a, v1, v3],[v2, a],[v3, a]]).
-jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a], [v4, e]], [[v1, e, v2],[v0, a, v1, v3],[v2, a],[v3, a]]).
+wyborWierzcholka([v0, a, v1, v3], B).
+wyborWierzcholka([v0, e, v1, v3], B).
 
 usunWierzcholek([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, C).
 usunWierzcholek([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, [v2, a]).
 usunWierzcholek([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, [v3, a]).
 
-znajdzWierzcholek([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], v0, B).
+jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B).
+jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], [[v0, a, v1, v3],[v1, e, v2],[v2, a],[v3, a]]).
+jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], [[v1, e, v2],[v0, a, v1, v3],[v2, a],[v3, a]]).
+jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a], [v4, e]], [[v1, e, v2],[v0, a, v1, v3],[v2, a],[v3, a]]).
+
+wezDowolnaNazwe([v0, v1, v2, v3, v4], B, C).
+
+znajdzWierzcholek([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, C).
 
 jestDFS([[v1, e, v2, v3], [v2, a], [v3, a]], B).
 jestDFS([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], B).
@@ -16,3 +21,5 @@ jestDFS([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], B).
 jestADFS([[v1, e, v2, v3], [v2, a], [v3, a]], B).
 jestADFS([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], B).
 jestADFS([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], B).
+
+jestADFS1([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], B).
--- 3/rs361086.pl
+++ 3/rs361086.pl
@@ -3,10 +3,7 @@
 :- use_module(library(lists)).
 
 
-% wyborWierzcholka - przeksztalca wierzcholek AEgrafu na wierzcholek,
-% ktory moze nalezec do wyboru
 wyborWierzcholka([V, a | Reszta], [V, a | Reszta]).
-
 wyborWierzcholka([V, e], [V, e]).
 wyborWierzcholka([V, e, _R | _AEGraf], [V, e, _R]).
 wyborWierzcholka([V, e, _R | AEGraf], [V, e, S]) :-
@@ -24,7 +21,6 @@ jestWyborem(AEGraf, [Wierz | Graf]) :-
   jestWyborem(AENowy, Graf).
 
 
-
 wezDowolnaNazwe([Nazwa | Nazwy], Nazwy, Nazwa).
 wezDowolnaNazwe([Nazwa1 | Nazwy1], [Nazwa1 | Nazwy2], Nazwa2) :-
   wezDowolnaNazwe(Nazwy1, Nazwy2, Nazwa2).
@@ -37,16 +33,16 @@ check(Name, Visited1, Visited2) :-
   nonmember(Name, Visited1),
   append(Visited1, [Name], Visited2).
 
-walkWithCheck(Graf, [V, T | Reszta], Visited, Visited2) :-
+walkWithCheck(Graf, [V, T, R | Reszta], Visited, Visited2) :-
   check(V, Visited, Visited1),
-  walk(Graf, [V, T | Reszta], Visited1, Visited2).
+  walk(Graf, [V, T, R | Reszta], Visited1, Visited2).
 walkWithCheck(_Graf, [V, _T], Visited, Visited1) :-
   check(V, Visited, Visited1).
 walkWithCheck(_Graf, [V | _Reszta], Visited, Visited) :-
   member(V, Visited).
 
-walk(Graf, [V, T | Reszta], Visited, Visited2) :-
-  wezDowolnaNazwe(Reszta, Reszta2, Nazwa),
+walk(Graf, [V, T, R | Reszta], Visited, Visited2) :-
+  wezDowolnaNazwe([R | Reszta], Reszta2, Nazwa),
   znajdzWierzcholek(Graf, Nazwa, Wierz),
   walkWithCheck(Graf, Wierz, Visited, Visited1),
   walk(Graf, [V, T | Reszta2], Visited1, Visited2).
@@ -56,11 +52,29 @@ jestDFS([G | Graf], Lista) :-
   walkWithCheck([G | Graf], G, [], Lista).
 
 
-
 jestADFS(AEgraf, Lista) :-
   jestWyborem(AEgraf, Graf),
   jestDFS(Graf, Lista).
 
 
+walkAEWithCheck(Graf, [V, T, R | Reszta], Visited, Visited2) :-
+  check(V, Visited, Visited1),
+  walkAE(Graf, [V, T, R | Reszta], Visited1, Visited2).
+walkAEWithCheck(_Graf, [V, _T], Visited, Visited1) :-
+  check(V, Visited, Visited1).
+walkAEWithCheck(_Graf, [V | _Reszta], Visited, Visited) :-
+  member(V, Visited).
 
-jestADFS1(_AEgraf, _Lista).
+walkAE(Graf, [V, a, R | Reszta], Visited, Visited2) :-
+  wezDowolnaNazwe([R | Reszta], Reszta2, Nazwa),
+  znajdzWierzcholek(Graf, Nazwa, Wierz),
+  walkAEWithCheck(Graf, Wierz, Visited, Visited1),
+  walkAE(Graf, [V, a | Reszta2], Visited1, Visited2).
+walkAE(Graf, [V, e, R | Reszta], Visited, Visited1) :-
+  wezDowolnaNazwe([R | Reszta], Reszta2, Nazwa),
+  znajdzWierzcholek(Graf, Nazwa, Wierz),
+  walkAEWithCheck(Graf, Wierz, Visited, Visited1).
+walkAE(_Graf, [_V, _T], Visited, Visited).
+
+jestADFS1([AE | AEgraf], Lista) :-
+  walkAEWithCheck([AE | AEgraf], AE, [], Lista).
