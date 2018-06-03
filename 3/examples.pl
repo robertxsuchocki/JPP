5e05c3937529c7b17a4bbfdae9ff0005a56fd48b
Robert Suchocki <rs361086@students.mimuw.edu.pl>
2018-06-03 23:45:02 +0200

    Multiplication reduced to acceptable levels, walks merged, commenting left

--- 3/examples.pl
+++ 3/examples.pl
@@ -1,24 +1,24 @@
-choiseOfNode([v0, a, v1, v3], B).
-B = [v0,a,v1,v3] ? ;
+choiseOfNode([v0, a, v1, v3], X).
+X = [v0,a,v1,v3] ? ;
 
-choiseOfNode([v0, e, v1, v3], B).
-B = [v0,e,v1] ? ;
-B = [v0,e,v3] ? ;
+choiseOfNode([v0, e, v1, v3], X).
+X = [v0,e,v1] ? ;
+X = [v0,e,v3] ? ;
 
 
-matchNodes([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, C).
-B = [[v1,e,v2,v3],[v2,a],[v3,a]],
-C = [v0,a,v1,v3] ? ;
+matchNodes([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], X, Y).
+X = [[v1,e,v2,v3],[v2,a],[v3,a]],
+Y = [v0,a,v1,v3] ? ;
 
-matchNodes([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, [v2, a]).
-B = [[v0,a,v1,v3],[v1,e,v2,v3],[v3,a]] ? ;
+matchNodes([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], X, [v2, a]).
+X = [[v0,a,v1,v3],[v1,e,v2,v3],[v3,a]] ? ;
 
-matchNodes([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, [v3, a]).
-B = [[v0,a,v1,v3],[v1,e,v2,v3],[v2,a]] ? ;
+matchNodes([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], X, [v3, a]).
+X = [[v0,a,v1,v3],[v1,e,v2,v3],[v2,a]] ? ;
 
-jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B).
-B = [[v0,a,v1,v3],[v1,e,v2],[v2,a],[v3,a]] ? ;
-B = [[v0,a,v1,v3],[v1,e,v3],[v2,a],[v3,a]] ? ;
+jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], X).
+X = [[v0,a,v1,v3],[v1,e,v2],[v2,a],[v3,a]] ? ;
+X = [[v0,a,v1,v3],[v1,e,v3],[v2,a],[v3,a]] ? ;
 
 
 jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], [[v0, a, v1, v3],[v1, e, v2],[v2, a],[v3, a]]).
@@ -31,62 +31,100 @@ jestWyborem([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a], [v4, e]], [[v1, e,
 no
 
 
-takeAnyName([v0, v1, v2, v3, v4], B, C).
-B = [v1,v2,v3,v4],
-C = v0 ? ;
-B = [v0,v2,v3,v4],
-C = v1 ? ;
-B = [v0,v1,v3,v4],
-C = v2 ? ;
-B = [v0,v1,v2,v4],
-C = v3 ? ;
-B = [v0,v1,v2,v3],
-C = v4 ? ;
+takeAnyName([v0, v1, v2, v3, v4], X, Y).
+X = [v1,v2,v3,v4],
+Y = v0 ? ;
+X = [v0,v2,v3,v4],
+Y = v1 ? ;
+X = [v0,v1,v3,v4],
+Y = v2 ? ;
+X = [v0,v1,v2,v4],
+Y = v3 ? ;
+X = [v0,v1,v2,v3],
+Y = v4 ? ;
 
 
-findNode([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], B, C).
-B = v0,
-C = [v0,a,v1,v3] ? ;
-B = v1,
-C = [v1,e,v2,v3] ? ;
-B = v2,
-C = [v2,a] ? ;
-B = v3,
-C = [v3,a] ? ;
+findNode([[v0, a, v1, v3],[v1, e, v2, v3],[v2, a],[v3, a]], X, Y).
+X = v0,
+Y = [v0,a,v1,v3] ? ;
+X = v1,
+Y = [v1,e,v2,v3] ? ;
+X = v2,
+Y = [v2,a] ? ;
+X = v3,
+Y = [v3,a] ? ;
 
 
-jestDFS([[v1, e, v2, v3], [v2, a], [v3, a]], B).
-B = [v1,v2,v3] ? ;
-B = [v1,v3,v2] ? ;
+jestDFS([[v1, e, v2, v3], [v2, a], [v3, a]], X).
+X = [v1,v2,v3] ? ;
+X = [v1,v3,v2] ? ;
 
-jestDFS([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], B).
-B = [v0,v1,v2,v3] ? ;
-B = [v0,v3,v1,v2] ? ;
+jestDFS([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], X).
+X = [v0,v1,v2,v3] ? ;
+X = [v0,v3,v1,v2] ? ;
 
-jestDFS([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], B).
-B = [v0,v1,v2,v3] ? ;
-B = [v0,v1,v3,v2] ? ;
-B = [v0,v3,v1,v2] ? ;
-B = [v0,v3,v1,v2] ? ;
-
-
-jestADFS([[v1, e, v2, v3], [v2, a], [v3, a]], B).
-B = [v1,v2] ? ;
-B = [v1,v3] ? ;
-
-jestADFS([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], B).
-B = [v0,v1,v2,v3] ? ;
-B = [v0,v3,v1,v2] ? ;
-
-jestADFS([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], B).
-B = [v0,v1,v2,v3] ? ;
-B = [v0,v3,v1,v2] ? ;
-B = [v0,v1,v3] ? ;
-B = [v0,v3,v1] ? ;
+jestDFS([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], X).
+X = [v0,v1,v2,v3] ? ;
+X = [v0,v1,v3,v2] ? ;
+X = [v0,v3,v1,v2] ? ;
 
 
-jestADFS1([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], B).
-B = [v0,v1,v2,v3] ? ;
-B = [v0,v1,v3] ? ;
-B = [v0,v3,v1,v2] ? ;
-B = [v0,v3,v1] ? ;
+jestADFS([[v1, e, v2, v3], [v2, a], [v3, a]], X).
+X = [v1,v2] ? ;
+X = [v1,v3] ? ;
+jestADFS1([[v1, e, v2, v3], [v2, a], [v3, a]], X).
+X = [v1,v2] ? ;
+X = [v1,v3] ? ;
+
+jestADFS([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], X).
+X = [v0,v1,v2,v3] ? ;
+X = [v0,v3,v1,v2] ? ;
+jestADFS1([[v0, a, v1, v3], [v1, e, v2], [v2, a], [v3, a]], X).
+X = [v0,v1,v2,v3] ? ;
+X = [v0,v3,v1,v2] ? ;
+
+jestADFS([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], X).
+X = [v0,v1,v2,v3] ? ;
+X = [v0,v3,v1,v2] ? ;
+X = [v0,v1,v3] ? ;
+X = [v0,v3,v1] ? ;
+jestADFS1([[v0, a, v1, v3], [v1, e, v2, v3], [v2, a], [v3, a]], X).
+X = [v0,v1,v2,v3] ? ;
+X = [v0,v1,v3] ? ;
+X = [v0,v3,v1] ? ;
+X = [v0,v3,v1,v2] ? ;
+
+jestADFS([[v0, a, v1, v3, v4], [v1, e, v2, v3, v4], [v2, a], [v3, a], [v4, a]], X).
+X = [v0,v1,v2,v3,v4] ? ;
+X = [v0,v1,v2,v4,v3] ? ;
+X = [v0,v3,v1,v2,v4] ? ;
+X = [v0,v3,v4,v1,v2] ? ;
+X = [v0,v4,v1,v2,v3] ? ;
+X = [v0,v4,v3,v1,v2] ? ;
+X = [v0,v1,v3,v4] ? ;
+X = [v0,v3,v1,v4] ? ;
+X = [v0,v3,v4,v1] ? ;
+X = [v0,v4,v1,v3] ? ;
+X = [v0,v4,v3,v1] ? ;
+X = [v0,v1,v4,v3] ? ;
+X = [v0,v3,v1,v4] ? ;
+X = [v0,v3,v4,v1] ? ;
+X = [v0,v4,v1,v3] ? ;
+X = [v0,v4,v3,v1] ? ;
+jestADFS1([[v0, a, v1, v3, v4], [v1, e, v2, v3, v4], [v2, a], [v3, a], [v4, a]], X).
+X = [v0,v1,v2,v3,v4] ? ;
+X = [v0,v1,v2,v4,v3] ? ;
+X = [v0,v1,v3,v4] ? ;
+X = [v0,v1,v4,v3] ? ;
+X = [v0,v3,v1,v4] ? ;
+X = [v0,v3,v1,v2,v4] ? ;
+X = [v0,v3,v1,v4] ? ;
+X = [v0,v3,v4,v1] ? ;
+X = [v0,v3,v4,v1] ? ;
+X = [v0,v3,v4,v1,v2] ? ;
+X = [v0,v4,v1,v3] ? ;
+X = [v0,v4,v1,v2,v3] ? ;
+X = [v0,v4,v1,v3] ? ;
+X = [v0,v4,v3,v1] ? ;
+X = [v0,v4,v3,v1] ? ;
+X = [v0,v4,v3,v1,v2] ? ;
--- 3/rs361086.pl
+++ 3/rs361086.pl
@@ -41,26 +41,37 @@ markVisited(Name, Visited1, Visited2) :-
   \+ member(Name, Visited1),
   append(Visited1, [Name], Visited2).
 
-walkWithCheck(Graph, [Name, Type, Next | Rest], Visited1, Visited3) :-
+walkWithCheck(Case, Graph, [Name, Type, Next | Rest], Visited1, Visited3) :-
   markVisited(Name, Visited1, Visited2),
-  walk(Graph, [Name, Type, Next | Rest], Visited2, Visited3).
-walkWithCheck(Graph, [Name, Type], Visited1, Visited2) :-
+  walk(Case, Graph, [Name, Type, Next | Rest], Visited2, Visited3).
+walkWithCheck(Case, Graph, [Name, Type], Visited1, Visited2) :-
   markVisited(Name, Visited1, Visited2).
-walkWithCheck(Graph, [Name | Rest], Visited, Visited) :-
+walkWithCheck(Case, Graph, [Name | Rest], Visited, Visited) :-
   member(Name, Visited).
 
-walk(Graph, [Name, Type, Next | Rest1], Visited, Visited) :-
-  skipVisited([Next | Rest1], Visited, []).
-walk(Graph, [CurrName, Type, Next | Rest1], Visited1, Visited3) :-
+walk(Case, Graph, [Name, Type], Visited, Visited).
+walk(Case, Graph, [Name, Type, Next | Rest], Visited, Visited) :-
+  skipVisited([Next | Rest], Visited, []).
+walk(Case, Graph, [CurrName, Type, Next | Rest1], Visited1, Visited3) :-
+  (Case == g; Type == a), !,
   skipVisited([Next | Rest1], Visited1, Rest2),
   takeAnyName(Rest2, Rest3, NextName),
   findNode(Graph, NextName, Node),
-  walkWithCheck(Graph, Node, Visited1, Visited2),
-  walk(Graph, [CurrName, Type | Rest3], Visited2, Visited3).
-walk(Graph, [Name, Type], Visited, Visited).
+  walkWithCheck(Case, Graph, Node, Visited1, Visited2),
+  walk(Case, Graph, [CurrName, Type | Rest3], Visited2, Visited3).
+walk(ae, Graph, [CurrName, e, Next | Rest1], Visited, Visited) :-
+  takeAnyName([Next | Rest1], Rest2, NextName),
+  member(NextName, Visited).
+walk(ae, Graph, [CurrName, e, Next | Rest1], Visited1, Visited2) :-
+  takeAnyName([Next | Rest1], Rest2, NextName),
+  \+ member(NextName, Visited1),
+  skipVisited(Rest2, Visited1, Rest3),
+  findNode(Graph, NextName, Node),
+  walkWithCheck(ae, Graph, Node, Visited1, Visited2).
+
 
 jestDFS([FirstNode | Graph], List) :-
-  walkWithCheck([FirstNode | Graph], FirstNode, [], List).
+  walkWithCheck(g, [FirstNode | Graph], FirstNode, [], List).
 
 
 jestADFS(AEGraph, List) :-
@@ -68,32 +79,5 @@ jestADFS(AEGraph, List) :-
   jestDFS(Graph, List).
 
 
-walkAEWithCheck(Graph, [Name, Type, Next | Rest], Visited, Visited2) :-
-  markVisited(Name, Visited, Visited1),
-  walkAE(Graph, [Name, Type, Next | Rest], Visited1, Visited2).
-walkAEWithCheck(Graph, [Name, Type], Visited, Visited1) :-
-  markVisited(Name, Visited, Visited1).
-walkAEWithCheck(Graph, [Name | Rest], Visited, Visited) :-
-  member(Name, Visited).
-
-walkAE(Graph, [Name, Type, Next | Rest], Visited, Visited) :-
-  skipVisited([Next | Rest], Visited, []).
-walkAE(Graph, [CurrName, a, Next | Rest], Visited1, Visited3) :-
-  skipVisited([Next | Rest], Visited1, Rest1),
-  takeAnyName(Rest1, Rest2, NextName),
-  findNode(Graph, NextName, Node),
-  walkAEWithCheck(Graph, Node, Visited1, Visited2),
-  walkAE(Graph, [CurrName, a | Rest2], Visited2, Visited3).
-walkAE(Graph, [CurrName, e, Next | Rest], Visited, Visited) :-
-  takeAnyName([Next | Rest], Rest1, NextName),
-  member(NextName, Visited).
-walkAE(Graph, [CurrName, e, Next | Rest], Visited1, Visited2) :-
-  takeAnyName([Next | Rest], Rest1, NextName),
-  \+ member(NextName, Visited1),
-  skipVisited(Rest1, Visited1, Rest2),
-  findNode(Graph, NextName, Node),
-  walkAEWithCheck(Graph, Node, Visited1, Visited2).
-walkAE(Graph, [Name, Type], Visited, Visited).
-
 jestADFS1([FirstNode | AEGraph], List) :-
-  walkAEWithCheck([FirstNode | AEGraph], FirstNode, [], List).
+  walkWithCheck(ae, [FirstNode | AEGraph], FirstNode, [], List).
